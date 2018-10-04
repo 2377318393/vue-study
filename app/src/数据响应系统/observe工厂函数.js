c2febd6ee2e212b0ccece3eb3e4038c6d4cd6362
princed <princed@princeddeMacBook-Pro.local>
2018-10-04 09:34:18 +0800

    1

--- app/src/数据响应系统/observe工厂函数.js
+++ app/src/数据响应系统/observe工厂函数.js
@@ -1,14 +1,16 @@
 import Vue from '../../../vue/dist/vue';
 
 const vm = new Vue({
-    data : {
-        a : 1,
-        test : 2,
-        location : {
-            city : 'hanghzou'
+    data: {
+        a: 1,
+        test: 2,
+        location: {
+            city: 'hanghzou'
         }
     }
 })
 
 vm.location
-vm.a = 3*'sdfsdf';
+vm.a = 3 * 'sdfsdf';
+
+
